95f9404c71816407f878fa036deedb5ac561c64b
Gavin Zhang <ZHANGGA7@OOCL.COM>
2025-09-14 14:46:46 +0800

    feat: add input and add button

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,17 +1,45 @@
-import {useContext} from 'react';
+import {useContext, useState} from 'react';
 import {TodoContext} from '../contexts/TodoContext';
 import './TodoList.css';
 
 const TodoList = () => {
   const {state, dispatch} = useContext(TodoContext);
+  const [inputText, setInputText] = useState('');
+
+  function toggleDone (id) {
+    const action = {type: 'DONE', id: id};
+    dispatch(action);
+  }
+
+  function addTodo () {
+    if (inputText.trim()) {
+      const action = {type: 'ADD', text: inputText};
+      dispatch(action);
+      setInputText('');
+    }
+  }
+
   return (
     <div className={'todo-group'}>
       <div>This is the TodoList Component.</div>
       {
-        state.map(todo => {
-          return <div className={'todo-item'}>{todo.text}</div>
+        state.map(({id, text, done}) => {
+          return <div className={`todo-item ${done ? 'done' : ''}`}
+                      onClick={() => toggleDone(id)}>{text}</div>
         })
       }
+      <input type='text'
+             value={inputText}
+             onChange={(e) => setInputText(e.target.value)}
+             className={'todo-input'}
+      />
+      <button
+        onClick={addTodo}
+        className={'todo-add-button'}
+        disabled={!inputText.trim()}
+      >
+        add
+      </button>
     </div>
   );
 }
--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -5,5 +5,29 @@ export const initialState = [
 
 // reducer is a pure function that define and gather all state update logic
 export const todoReducer = (state, action) => {
-  return state;
+  switch (action.type) {
+    case 'DONE':
+      return state.map(todo => {
+        if (action.id === todo.id) {
+          const done = !todo.done;
+          return {
+            ...todo,
+            done: done
+          };
+        }
+        return todo;
+      })
+    case 'ADD':
+      const newId = Math.max(...state.map(todo => todo.id)) + 1;
+      return [
+        ...state,
+        {
+          id: newId,
+          text: action.text,
+          done: false
+        }
+      ];
+    default:
+      return state;
+  }
 };
