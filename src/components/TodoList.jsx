399bd506667da48e618910171a0b24059cf9905b
Gavin Zhang <ZHANGGA7@OOCL.COM>
2025-09-15 14:31:41 +0800

    feat: add TodoGroup

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,86 +1,9 @@
-import {
-  useContext,
-  useEffect,
-  useState
-} from 'react';
-import {TodoContext} from '../contexts/TodoContext';
 import './TodoList.css';
-import {getTodos} from '../apis/api';
+import TodoGroup from './TodoGroup';
 
 const TodoList = () => {
-  const {state, dispatch} = useContext(TodoContext);
-  const [inputText, setInputText] = useState('');
-
-  useEffect(() => {
-    getTodos().then(response => {
-      dispatch({type: 'LOAD', todos: response.data});
-    })
-  }, []);
-
-  function toggleDone (id) {
-    const action = {type: 'DONE', id: id};
-    dispatch(action);
-  }
-
-  function addTodo () {
-    if (inputText.trim()) {
-      const action = {type: 'ADD', text: inputText};
-      dispatch(action);
-      setInputText('');
-    }
-  }
-
-  function deleteTodo (id, e) {
-    e.stopPropagation();
-    const action = {type: 'DELETE', id: id};
-    dispatch(action);
-  }
-
   return (
-    <div className={'todo-group'}>
-      <div className='todo-title'>Todo List</div>
-      {
-        state.map(({
-                     id,
-                     text,
-                     done
-                   }) => {
-          return (
-            <div className='todo-row'>
-              <div className={`todo-item ${done ? 'done' : ''}`}
-                   onClick={() => toggleDone(id)}>
-                {text}
-              </div>
-              <button
-                className='delete-button'
-                onClick={(e) => deleteTodo(id, e)}
-              >
-                X
-              </button>
-            </div>
-          )
-        })
-      }
-      {
-        state.length === 0 && (
-          <div className="todo-empty-info">
-            Add the things you need to do today...
-          </div>
-        )
-      }
-      <input type='text'
-             value={inputText}
-             onChange={(e) => setInputText(e.target.value)}
-             className={'todo-input'}
-      />
-      <button
-        onClick={addTodo}
-        className={'todo-add-button'}
-        disabled={!inputText.trim()}
-      >
-        add
-      </button>
-    </div>
+    <TodoGroup/>
   );
 }
 
